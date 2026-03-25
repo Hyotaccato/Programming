c4b1b744cb1ac1e94f8fb76ca4c77bd3091e7dbb
Lee Hyo-eun <hi285283@gmail.com>
2026-03-26 08:43:10 +0900

    counter HTML, JS id, class

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -1,8 +1,10 @@
 // const h1 = document.getElementsByTagName('h1')[0];
 // const h1 = document.querySelectorAll('h1')[0];
-const h1 = document.querySelector('h1');
-
-const plusButton = document.querySelector('button');
+// const h1 = document.querySelector('h1');
+const h1 = document.getElementById('result');
+// const plusButton = document.querySelector('button');
+// const plusButton = document.querySelector('.plusButton')[0];
+const plusButton = document.querySelectorAll('.plusButton');
 
 let count = 0;
 
